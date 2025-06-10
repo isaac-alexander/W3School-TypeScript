310507035874e62b060a8656c71322514d3ae7c4
Isaac Alexander <Isaacalexander81@gmail.com>
2025-06-10 01:52:18 +0100

    aliasesAndInterfaces.js

---
+++ aliasesAndInterfaces.js
@@ -0,0 +1,24 @@
+"use strict";
+// TypeScript Type Aliases and Interfaces
+// TypeScript allows types to be defined separately from the variables that use them.
+const carYear = 2001;
+const carType = "Toyota";
+const carModel = "Corolla";
+const car = {
+    year: carYear,
+    type: carType,
+    model: carModel
+};
+console.log(car);
+;
+const rectangle = {
+    height: 20,
+    width: 10
+};
+console.log(rectangle);
+const coloredRectangle = {
+    height: 20,
+    width: 10,
+    color: "red"
+};
+console.log(coloredRectangle);
