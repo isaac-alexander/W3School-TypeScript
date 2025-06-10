b068a1f639acc3b82251bea8844fdd9d994fed21
Isaac Alexander <Isaacalexander81@gmail.com>
2025-06-10 01:51:52 +0100

    objectTypes.ts

--- objectTypes.ts
+++ objectTypes.ts
@@ -6,13 +6,13 @@
 // Example
 
 // Try playing around with modifying properties and adding ones to see what happens
-const car: { type: string, model: string, year: number } = {
+const carA: { type: string, model: string, year: number } = {
     type: "Toyota",
     model: "Corolla",
     year: 2009
 };
 
-console.log(car);
+console.log(carA);
 
 ///////////////////////////////////////////////////////////////////
 
